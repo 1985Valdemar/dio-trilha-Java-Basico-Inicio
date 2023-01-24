a91b58df5d344e0d9bea7eaf8872f50319d98b00
vteider <vteider@gmail.com.br>
2023-01-24 05:58:24 -0300

    SmartTv - configuracoes - inicial

---
+++ SISTEMA-SMART-TV/src/SmartTv.java
@@ -0,0 +1,13 @@
+public class SmartTv {
+    boolean ligado = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar() {
+        ligado = true;
+    }
+
+    public void desligar() {
+        ligado = false;
+    }
+}
