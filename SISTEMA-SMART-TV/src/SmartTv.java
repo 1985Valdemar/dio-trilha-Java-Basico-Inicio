1a1ef34c0bbf64d412bdf585dd6d76a24400d510
vteider <vteider@gmail.com.br>
2023-01-25 05:59:42 -0300

    Parametrização Usuario+SmarTv

--- SISTEMA-SMART-TV/src/SmartTv.java
+++ SISTEMA-SMART-TV/src/SmartTv.java
@@ -3,6 +3,32 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+    public void aumentarVolume() {
+        // Pode ser assim volume = volume + 1; ou abaixo
+        volume++;
+
+        System.out.println("Aumentando o volume para: " + volume);
+
+    }
+
+    public void diminuirVolume() {
+        // Pode ser assim volume = volume - 1; ou abaixo
+        volume--;
+        System.out.println("Diminuido o volume para: " + volume);
+    }
+
     public void ligar() {
         ligado = true;
     }
