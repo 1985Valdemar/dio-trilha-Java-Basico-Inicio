12aa3e98e476f8690605fb0a73f21f62c1e1f91f
vteider <vteider@gmail.com.br>
2023-01-26 06:02:44 -0300

    Inicio calculadora

---
+++ JAVA-TERMINAL/src/App.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>calculadora</h1>
+ * A calculadora realiza operações matematicas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Valdemar Teider
+ * @version 1.0
+ * @since 26/01/2023
+ */
+public class calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * 
+     * @param numeroUm   este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numerosUm, int numeroDois) {
+        return numerosUm + numeroDois;
+    }
+
+}
