c80ccda99a662bf278da0a8676279556ceef0980
vteider <vteider@gmail.com.br>
2023-01-30 05:36:49 -0300

    Comentarios Tipos

---
+++ JAVA-TERMINAL/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentario em uma única linha
+
+        /*
+         * eu sou um comentario
+         * que posso ser mais datalhado
+         * quando necessario
+         */
+    }
+
+    /**
+     * Estas Duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação
+     * Que incrivel!!
+     */
+    public void metodo() {
+
+    }
+}
