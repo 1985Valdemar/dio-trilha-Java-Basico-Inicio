bda5f63d1a05d0ce36e7a977a021e575a43e1604
vteider <vteider@gmail.com.br>
2023-02-07 05:53:05 -0300

    CONDICIONAL COMPOSTA

---
+++ Controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+// CONDICIONAL COMPOSTA MAIS DE UMA VARIAVEL 
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 8;
+
+        // SE NOTA MAIOR QUE 7
+        if (nota >= 7)
+            System.out.println("APROVADO");
+
+        // SE NÃO NOTA MENOR QUE 7
+        else
+            System.out.println("REPROVADO");
+
+    }
+
+}
